167ea4d15f2c9cac8ee95366a243f15c51d4c1e1
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-10-25 20:50:55 +0000

    Added category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -9,22 +9,32 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
+    # To ensure we don't get an error when
+    # loading the products page without a search term
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter a search criterial!")
-                return redirect(reverse('products'))
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+        if request.GET:
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search criterial!")
+                    return redirect(reverse('products'))
+
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
